771af4f0b45104aeff086182c5ce8f3631523ab4
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-09-08 18:27:43 +0000

    changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,8 +13,8 @@ public class PracticeProblem {
 		//Write question 1 code here "Get a String using "In: " as the prompt. Output the string."
 		Scanner reader = new Scanner(System.in);
 		System.out.print("Int: ");
-		String prompt = reader.nextLine();
-		System.out.println(prompt);
+		int prompt = reader.nextInt();
+		System.out.println(prompt); //prints input by user
 	}
 
 	public static void q2() {
@@ -24,16 +24,16 @@ public class PracticeProblem {
 		int integer1 = reader.nextInt();
 		System.out.print("Int 2: ");
 		int integer2 = reader.nextInt();
-		System.out.println(integer1 / integer2);
-		System.out.println(integer1 % integer2);
+		System.out.println(integer1 / integer2); //prints division
+		System.out.println(integer1 % integer2); //prints remainder
 		
 	}
 
 	public static void q3() {
 		//Write question 3 code here "'Get a String using "In: " as the prompt. Output the String + " was the text you wrote" on the same line."
 		Scanner reader = new Scanner(System.in);
-		System.out.print("In: ");
-		String integerprompt = reader.nextLine();
+		System.out.print("In: "); //prints in:
+		String integerprompt = reader.nextLine(); //reads input from user
 		System.out.println(integerprompt + " was the text you wrote on the same line.");
 	}
 
@@ -42,7 +42,7 @@ public class PracticeProblem {
 		Scanner reader = new Scanner(System.in);
 		System.out.print("In: ");
 		int integerinput = reader.nextInt();
-		System.out.println(integerinput * 5);
+		System.out.println(integerinput * 5); //multiply input by 5
 	}
 
 	public static void q5() {
@@ -50,15 +50,15 @@ public class PracticeProblem {
 		Scanner reader = new Scanner(System.in);
 		System.out.print("In: ");
 		boolean booleaninput = reader.nextBoolean();
-		System.out.println(booleaninput + " is a boolean");
-	}
+		System.out.println(booleaninput + " is a boolean"); //prints the input with "is a boolean" after	
+		}
 
 	public static void q6() {
 		//Write question 6 code here "Get a double using "In: " as the prompt. Output the double - 3.2."
 		Scanner reader = new Scanner(System.in);
 		System.out.print("In: ");
 		double doubleinput = reader.nextDouble();
-		System.out.println(doubleinput);
+		System.out.println(doubleinput); //prints the double
 	}
 
 }
